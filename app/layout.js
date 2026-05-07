62987fd1d13163c043ddcb27b19cf6ec2f420f2d
Zasif Bin Islam <zasifbinislam@gmail.com>
2026-05-08 04:30:56 +0600

    SSR: default html lang=bn so Bengali fonts apply on first paint

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="bn">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
